7c3e2322de4602af493cd1470e9e785a55ebc803
jaxer20 <119001942+jaxer20@users.noreply.github.com>
2023-01-28 01:46:11 +0300

    objects

---
+++ objects.js
@@ -0,0 +1,39 @@
+ //? nested
+const people = {
+    person1: {
+      name: "Can",
+      surname: "Canan",
+      dob: "1990",
+      job: "developer",
+      salary: "140000",
+      drivingLicense: true,
+    },
+    person2: {
+      name: "John",
+      surname: "Sweet",
+      dob: "1990",
+      job: "tester",
+      salary: "110000",
+      drivingLicense: false,
+    },
+    person3: {
+      name: "Steve",
+      surname: "Job",
+      dob: "2000",
+      job: "developer",
+      salary: "90000",
+      drivingLicense: true,
+    },
+    person4: "JS",
+  }
+  
+  //! ASSIGMENT
+  //? person2'nin adini ve maasini yazdiriniz.
+  console.log(people)
+  console.log(people.person2.name)
+  console.log(people.person2.salary)
+  
+  //? people objesindeki tum salary 'leri yazdirin (Dongu kullanilmal
+
+  
+  
